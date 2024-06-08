09b3a4927ff9e5690f40c859897be99217e47c0b
Ridez <ridez12@gmail.com>
2024-06-08 09:49:23 +0200

    feat: add truck form.

---
+++ components/trucks/TruckForm/index.tsx
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import StatusSelect from "@/components/StatusSelect";
+import { useAddTruckMutation, useUpdateTruckMutation } from "@/store/api";
+import { TruckStatus } from "@/types/trucks/truckStatus";
+import { TruckData } from "@/types/trucks/truckData";
+
+interface TruckFormProps {
+  initialData?: TruckData;
+  onClose: () => void;
+  isEditMode: boolean;
+}
+
+const TruckForm: React.FC<TruckFormProps> = ({
+  initialData,
+  onClose,
+  isEditMode,
+}) => {
+  const { control, handleSubmit, reset, setError } = useForm<TruckData>({
+    defaultValues: initialData || {
+      code: "",
+      name: "",
+      status: undefined,
+      description: "",
+    },
+  });
+
+  const [initialStatus, setInitialStatus] = useState<TruckStatus>(
+    initialData?.status || TruckStatus.LOADING
+  );
+
+  const [
+    addTruck,
+    {
+      error: errorCreateTruck,
+      isSuccess: isSuccessCreateTruck,
+      isLoading: isLoadingCreateTruck,
+    },
+  ] = useAddTruckMutation<any>();
+
+  const [
+    updateTruck,
+    {
+      error: errorUpdateTruck,
+      isSuccess: isSuccessUpdateTruck,
+      isLoading: isLoadingUpdateTruck,
+    },
+  ] = useUpdateTruckMutation<any>();
+
+  useEffect(() => {
+    if (initialData) {
+      setInitialStatus(initialData.status || "");
+      reset(initialData);
+    }
+  }, [initialData, reset]);
+
+  useEffect(() => {
+    if (
+      errorCreateTruck?.data?.code === "NOT_UNIQUE" ||
+      errorUpdateTruck?.data?.code === "NOT_UNIQUE"
+    ) {
+      setError("code", {
+        type: "manual",
+        message: "Truck code must be unique.",
+      });
+    }
+  }, [errorCreateTruck, errorUpdateTruck, setError]);
+
+  const onSubmit = (data: TruckData) => {
+    if (isEditMode) {
+      updateTruck({ id: data.id, requestBody: data });
+    } else {
+      addTruck({ requestBody: data });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="code"
+        control={control}
+        rules={{
+          required: "Truck code is required.",
+          pattern: {
+            value: /^[a-zA-Z0-9]+$/,
+            message: "Code must be alphanumeric.",
+          },
+        }}
+        render={({
+          field: { onChange, ref, value, name },
+          fieldState: { error },
+        }) => (
+          <Input
+            label="Code"
+            error={!!error}
+            errorMessage={error?.message}
+            onChange={onChange}
+            ref={ref}
+            value={value}
+            id={name}
+            className="mb-4"
+          />
+        )}
+      />
+
+      <Controller
+        name="name"
+        control={control}
+        rules={{
+          required: "Name is required.",
+        }}
+        render={({
+          field: { onChange, ref, value, name },
+          fieldState: { error },
+        }) => (
+          <Input
+            label="Name"
+            error={!!error}
+            errorMessage={error?.message}
+            onChange={onChange}
+            ref={ref}
+            value={value}
+            id={name}
+            className="mb-4"
+          />
+        )}
+      />
+
+      <Controller
+        name="status"
+        control={control}
+        rules={{
+          required: "Status is required.",
+        }}
+        render={({
+          field: { onChange, ref, value, name },
+          fieldState: { error },
+        }) => (
+          <div className="mb-4">
+            <label className="pb-3 text-base font-medium text-neutral-20 transition-all duration-300 ease-in-out pointer-events-none">
+              Status
+            </label>
+            <StatusSelect
+              value={value}
+              initialStatus={initialStatus}
+              onChange={onChange}
+            />
+            {error && <p className="text-red-600 text-sm">{error.message}</p>}
+          </div>
+        )}
+      />
+
+      <Controller
+        name="description"
+        control={control}
+        render={({
+          field: { onChange, ref, value, name },
+          fieldState: { error },
+        }) => (
+          <Input
+            label="Description"
+            error={!!error}
+            errorMessage={error?.message}
+            onChange={onChange}
+            ref={ref}
+            value={value}
+            id={name}
+            type="textarea"
+            optional
+          />
+        )}
+      />
+
+      <div className="block mt-4">
+        <Button onClick={onClose} variant="secondary" className="mr-2">
+          Close
+        </Button>
+        <Button type="submit" variant="primary">
+          {isEditMode ? "Update" : "Add"}
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default TruckForm;
