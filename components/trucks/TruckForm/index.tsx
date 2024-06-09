799059d2502271444a8fbc4869d444ac337927d2
Ridez <ridez12@gmail.com>
2024-06-10 00:04:02 +0200

    feat: handle unique code error.

--- components/trucks/TruckForm/index.tsx
+++ components/trucks/TruckForm/index.tsx
@@ -31,6 +31,8 @@ const TruckForm: React.FC<TruckFormProps> = ({
     initialData?.status
   );
 
+  const [isUniqueCodeError, setIsUniqueCodeError] = useState(false);
+
   const [
     addTruck,
     {
@@ -58,7 +60,7 @@ const TruckForm: React.FC<TruckFormProps> = ({
 
   useEffect(() => {
     if (initialData) {
-      setInitialStatus(initialData.status || "");
+      setInitialStatus(initialData.status || undefined);
       reset(initialData);
     }
   }, [initialData, reset]);
@@ -72,6 +74,9 @@ const TruckForm: React.FC<TruckFormProps> = ({
         type: "manual",
         message: "Truck code must be unique.",
       });
+      setIsUniqueCodeError(true);
+    } else {
+      setIsUniqueCodeError(false);
     }
   }, [errorCreateTruck, errorUpdateTruck, setError]);
 
@@ -85,127 +90,124 @@ const TruckForm: React.FC<TruckFormProps> = ({
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {!isSuccessCreateTruck &&
-        !isSuccessUpdateTruck &&
-        !isErrorCreateTruck &&
-        !isErrorUpdateTruck && (
-          <>
-            <Controller
-              name="code"
-              control={control}
-              rules={{
-                required: "Truck code is required.",
-                pattern: {
-                  value: /^[a-zA-Z0-9]+$/,
-                  message: "Code must be alphanumeric.",
-                },
-              }}
-              render={({
-                field: { onChange, ref, value, name },
-                fieldState: { error },
-              }) => (
-                <Input
-                  label="Code"
-                  error={!!error}
-                  errorMessage={error?.message}
-                  onChange={onChange}
-                  ref={ref}
+      {!isSuccessCreateTruck && !isSuccessUpdateTruck && (
+        <>
+          <Controller
+            name="code"
+            control={control}
+            rules={{
+              required: "Truck code is required.",
+              pattern: {
+                value: /^[a-zA-Z0-9]+$/,
+                message: "Code must be alphanumeric.",
+              },
+            }}
+            render={({
+              field: { onChange, ref, value, name },
+              fieldState: { error },
+            }) => (
+              <Input
+                label="Code"
+                error={!!error}
+                errorMessage={error?.message}
+                onChange={onChange}
+                ref={ref}
+                value={value}
+                id={name}
+                className="mb-4"
+              />
+            )}
+          />
+
+          <Controller
+            name="name"
+            control={control}
+            rules={{
+              required: "Name is required.",
+            }}
+            render={({
+              field: { onChange, ref, value, name },
+              fieldState: { error },
+            }) => (
+              <Input
+                label="Name"
+                error={!!error}
+                errorMessage={error?.message}
+                onChange={onChange}
+                ref={ref}
+                value={value}
+                id={name}
+                className="mb-4"
+              />
+            )}
+          />
+
+          <Controller
+            name="status"
+            control={control}
+            rules={{
+              required: "Status is required.",
+            }}
+            render={({
+              field: { onChange, ref, value, name },
+              fieldState: { error },
+            }) => (
+              <div className="mb-4">
+                <label className="pb-3 text-base font-medium text-neutral-20 transition-all duration-300 ease-in-out pointer-events-none">
+                  Status
+                </label>
+                <StatusSelect
                   value={value}
-                  id={name}
-                  className="mb-4"
-                />
-              )}
-            />
-
-            <Controller
-              name="name"
-              control={control}
-              rules={{
-                required: "Name is required.",
-              }}
-              render={({
-                field: { onChange, ref, value, name },
-                fieldState: { error },
-              }) => (
-                <Input
-                  label="Name"
-                  error={!!error}
-                  errorMessage={error?.message}
+                  initialStatus={initialStatus}
                   onChange={onChange}
-                  ref={ref}
-                  value={value}
-                  id={name}
-                  className="mb-4"
                 />
-              )}
-            />
+                {error && (
+                  <p className="text-red-600 text-sm">{error.message}</p>
+                )}
+              </div>
+            )}
+          />
 
-            <Controller
-              name="status"
-              control={control}
-              rules={{
-                required: "Status is required.",
-              }}
-              render={({
-                field: { onChange, ref, value, name },
-                fieldState: { error },
-              }) => (
-                <div className="mb-4">
-                  <label className="pb-3 text-base font-medium text-neutral-20 transition-all duration-300 ease-in-out pointer-events-none">
-                    Status
-                  </label>
-                  <StatusSelect
-                    value={value}
-                    initialStatus={initialStatus}
-                    onChange={onChange}
-                  />
-                  {error && (
-                    <p className="text-red-600 text-sm">{error.message}</p>
-                  )}
-                </div>
-              )}
-            />
+          <Controller
+            name="description"
+            control={control}
+            render={({
+              field: { onChange, ref, value, name },
+              fieldState: { error },
+            }) => (
+              <Input
+                label="Description"
+                error={!!error}
+                errorMessage={error?.message}
+                onChange={onChange}
+                ref={ref}
+                value={value}
+                id={name}
+                type="textarea"
+                optional
+              />
+            )}
+          />
 
-            <Controller
-              name="description"
-              control={control}
-              render={({
-                field: { onChange, ref, value, name },
-                fieldState: { error },
-              }) => (
-                <Input
-                  label="Description"
-                  error={!!error}
-                  errorMessage={error?.message}
-                  onChange={onChange}
-                  ref={ref}
-                  value={value}
-                  id={name}
-                  type="textarea"
-                  optional
-                />
-              )}
-            />
-
-            <div className="block mt-4">
-              <Button onClick={onClose} variant="secondary" className="mr-2">
-                Close
-              </Button>
-              <Button type="submit" variant="primary">
-                {isEditMode ? "Update" : "Add"}
-              </Button>
-              {isLoadingCreateTruck && (
-                <span className="ml-2">Creating truck...</span>
-              )}
-              {isLoadingUpdateTruck && (
-                <span className="ml-2">Updateing truck...</span>
-              )}
-            </div>
-          </>
-        )}
+          <div className="block mt-4">
+            <Button onClick={onClose} variant="secondary" className="mr-2">
+              Close
+            </Button>
+            <Button type="submit" variant="primary">
+              {isEditMode ? "Update" : "Add"}
+            </Button>
+            {isLoadingCreateTruck && (
+              <span className="ml-2">Creating truck...</span>
+            )}
+            {isLoadingUpdateTruck && (
+              <span className="ml-2">Updating truck...</span>
+            )}
+          </div>
+        </>
+      )}
       {isSuccessCreateTruck && (
         <>
-          <p>Truck successfuly created.</p>
+          <p>Truck successfully created.</p>
           <div className="block mt-4">
             <Button variant="primary" onClick={onClose}>
               Ok
@@ -216,7 +218,7 @@ const TruckForm: React.FC<TruckFormProps> = ({
 
       {isSuccessUpdateTruck && (
         <>
-          <p>Truck successfuly updated.</p>
+          <p>Truck successfully updated.</p>
           <div className="block mt-4">
             <Button variant="primary" onClick={onClose}>
               Ok
@@ -225,7 +227,7 @@ const TruckForm: React.FC<TruckFormProps> = ({
         </>
       )}
 
-      {(isErrorCreateTruck || isErrorUpdateTruck) && (
+      {(isErrorCreateTruck || isErrorUpdateTruck) && !isUniqueCodeError && (
         <>
           <p>Something went wrong, please try again.</p>
           <div className="block mt-4">
